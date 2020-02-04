2c2d6c1377f879342d36cba37dd46c7c3c794dc6
Cédric Chounavelle <cedricchounavelle@gmail.com>
2020-02-04 11:05:05 +0100

    Create custom abstract form factory

---
+++ src/Form/Elements/Field.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+    FormInput,
+} from './Style';
+
+const Field = (
+    {field: {
+        name,
+        label,
+        type,
+        value,
+        touched,
+        errors
+    },
+    changeHandler}
+               ) => {
+
+    const isError = undefined !== errors && touched;
+
+    return (
+            <div className={`form-item${isError ? ' has-error' : ''}`}>
+                <label htmlFor="">{label}</label>
+                <FormInput type={type} value={value}
+                       onChange={(e) => changeHandler(e, name)} />
+                {isError && <span className="error-message">{errors.map((err, k) => <span key={k} className="error-item">{err}</span>)}</span>}
+            </div>
+    );
+};
+
+export default Field;
---
+++ src/Form/Elements/Form.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {useFormHandler} from "../../Hooks";
+import Field from "./Field";
+
+import {
+    SubmitButton
+} from './Style';
+
+const Form = ({ fields, submitButtonValue, validation, children }) => {
+
+    const {
+        values,
+        errors,
+        touched,
+        handleInputChange,
+        submit
+    } = useFormHandler(fields);
+
+    fields = fields.map(field => ({
+        ...field,
+        value: values[field.name],
+        touched: touched[field.name],
+        errors: errors[field.name]
+    }));
+
+    const isFormError = undefined !== errors.form;
+
+    return (
+        <form onSubmit={(e) => submit(e, validation)}>
+
+            {fields.map(
+                (field, i) => <Field field={field} changeHandler={handleInputChange} key={i}/>
+            )}
+
+            {children}
+
+            <div className={`form-item${isFormError ? ' has-error' : ''}`}>
+                {isFormError && <span className="error-message">{errors.form.map((err, k) => <span key={k} className="error-item">{err}</span>)}</span>}
+            </div>
+            <div className="form-item">
+                <SubmitButton type="submit" value={submitButtonValue} />
+            </div>
+        </form>
+    );
+};
+
+export default Form;
